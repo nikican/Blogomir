938a572566bfeef0071016d217c1edc7c4f3c5ff
Nikica <novakovicnikica@gmail.com>
2016-12-23 14:21:02 +0000

    App initialised Index page created

---
+++ app.js
@@ -0,0 +1,50 @@
+var bodyParser  = require("body-parser"),
+mongoose        = require("mongoose"),
+express         = require("express"),
+app             = express();
+
+//DB connection
+mongoose.connect("mongodb://localhost/blogomir");
+
+//template engine
+app.set("view engine", "ejs");
+
+//body parser
+app.use(bodyParser.urlencoded({encoded:true}));
+
+//serve "public" folder
+app.use(express.statitc("public"));
+
+//mongoose/model config
+var blogSchema = new mongoose.Schema({
+    name:String,
+    image:String,
+    body:String,
+    date: {type: Date, defualt:Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+// RESTful routes
+
+//landing
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+});
+
+//index
+app.get("/blogs", function(req, res){
+    Blog.find({},function(error, blogs){
+        if(!error){
+            console.log(`Blog retrieved ${blogs}`)
+            res.render("blogIndex", {blogs:blogs});
+        }else{
+            console.log("Retrieve error!");
+        }
+    });  
+});
+
+//start server with C9 IP and port
+app.listen(process.env.PORT, process.env.IP, function (){
+    console.log(`Server started at ${process.env.IP}:${process.env.PORT}`);
+});
